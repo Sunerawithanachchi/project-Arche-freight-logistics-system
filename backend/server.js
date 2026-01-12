a9a6229a7c2eb725f874986b392d65f526c2ca8a
Sunerawithanachchi <sunerawithanachchi@gmail.com>
2026-01-12 19:37:00 +0530

    feat: implement dotenv and environment-aware server startup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const app = require("./app");
-const PORT = 3000;
 
 // Start the server (Opening the building)
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || "development";
+
 app.listen(PORT, () => {
   console.log(
-    "Arche freight logistics system Server is running at http://localhost:${PORT}"
+    `Arche freight logistics system Server is running in ${ENV} mode on PORT:${PORT}`
   );
 });
